ddf9bc4fe7f0d94969b7564af3e543c0039c68e9
LRThornton <shodge51@aol.com>
2022-12-19 12:03:52 -0500

    Added user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user.class';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
-  constructor() { }
+export class UserService {
+//baseUrl: string = "http://172.20.131.5:90/QuoteTool/api/users";
+baseUrl: string = "http://localhost:5000/api/users";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(usr: string, pwd: string): Observable<User> {
+    return this.http.get(`${this.baseUrl}/${usr}/${pwd}`) as Observable<User>;
+  }
+  list(): Observable<User[]> {
+    return this.http.get(`${this.baseUrl}`) as Observable<User[]>;
+  }
+  get(id: number): Observable<User> {
+    return this.http.get(`${this.baseUrl}/${id}`) as Observable<User>;
+  }
+  create(user: User): Observable<User> {
+    return this.http.post(`${this.baseUrl}`, user) as Observable<User>;
+  }
+  change(user: User): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${user.id}`, user) as Observable<any>;
+  }
+  remove(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`) as Observable<User>;
+  }
 }
