61b7b4bb78e8c9b58113ce0fce4305daedde31c4
LRThornton <shodge51@aol.com>
2022-12-19 13:21:18 -0500

    Added system service and userLogin.ts code

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SystemService } from 'src/app/system.service';
+import { UserService } from '../user.service';
+
+
 
 @Component({
   selector: 'app-user-login',
@@ -7,7 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  showVerifyButton: boolean = false;
+  get isAdmin() {
+    if(!this.sys.isLoggedIn) {
+      return false;
+    }
+    return this.sys.getLoggedInUser()?.isAdmin;
+  }
+
+  usr: string = "vv";
+  pwd: string = "vv";
+  msg: string = "";
+
+  constructor(
+    private sys: SystemService,
+    private usrsvc: UserService,
+    private router: Router
+  ) { }
+
+  login(): void {
+    this.msg = "";
+    this.usrsvc.login(this.usr, this.pwd).subscribe({
+      next: (res)=> {
+        console.debug("Login Successful");
+        this.sys.setLoggedInUser(res);
+        this.router.navigateByUrl("/quoterequests/list");
+      },
+      error: (err) => {
+        if(err.status == 404) {
+          this.msg = "Username/Password is not found";
+        }  else {
+           console.error(err);
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
